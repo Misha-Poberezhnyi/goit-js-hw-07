7f390dcf0e21fa95186757fa20ae59415492d172
Misha_Pobereznyi <Pober1997@gmail.com>
2025-03-03 21:52:31 +0200

    hw7task1.2.3

--- js/task-2.js
+++ js/task-2.js
@@ -24,3 +24,17 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+
+const gallery = document.querySelector(".gallery");
+
+const galleryItems = images.map(image => {
+  const li = document.createElement("li");
+  const img = document.createElement("img");
+  img.src = image.url;
+  img.alt = image.alt;
+  li.appendChild(img);
+  return li;
+});
+
+gallery.append(...galleryItems);
+
