d13732dc411673bb3fd354980ca3e42738162e2d
Misha_Pobereznyi <Pober1997@gmail.com>
2025-03-04 23:14:54 +0200

    HW7

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,12 @@
-  const nameInput = document.getElementById('name-input');
-  const nameOutput = document.getElementById('name-output');
+  const nameInput = document.getElementById("name-input");
+  const nameOutput = document.getElementById("name-output");
 
-  nameInput.addEventListener('input', () => {
+  nameInput.addEventListener("input", () => {
     
     let name = nameInput.value.trim();
 
-    if (name === '') {
-      nameOutput.textContent = 'Anonymous';
+    if (name === "") {
+      nameOutput.textContent = "Anonymous";
     } else {
       nameOutput.textContent = name;
     }
